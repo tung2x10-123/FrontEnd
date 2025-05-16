b9b46e3710a4a87e41c765a4af18191ced8417df
tungnt26 <tungnt26@lienvietpostbank.com.vn>
2025-05-16 15:24:13 +0700

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import {ChatbotComponent} from './chatbot/chatbot.component';
+import {FooterComponent} from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ChatbotComponent],
+  imports: [RouterOutlet, NavbarComponent, ChatbotComponent, FooterComponent],
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   styles: []
 })
 export class AppComponent {}
